df91cc1cfb491c2bb217d494c93ebcee5b30849b
박현민 <dev.hmpark@gmail.com>
2024-02-21 22:30:35 +0900

    typescript 로 변경

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import { Reset } from "styled-reset";
+function App() {
+  return (
+    <>
+      <Reset />
+      <div>hi</div>
+    </>
+  );
+}
+
+export default App;
